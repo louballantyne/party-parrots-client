ea0786a23c89dd65f4c49b6a597d8529b3a33d46
Sha <cspoppuppy@hotmail.com>
2021-05-11 16:56:33 +0100

    sign up form

--- components/signup.js
+++ components/signup.js
@@ -1,31 +1,67 @@
-import React from "react";
-import { StyleSheet, TextInput, Button, SafeAreaView, View } from "react-native";
+import React from 'react';
+import { StyleSheet, TextInput, Button, SafeAreaView, View } from 'react-native';
 
 const SignUp = () => {
-  const [text, onChangeText] = React.useState();
+	const [firstName, setFirstName] = React.useState();
+	const [lastName, setLastName] = React.useState();
+	const [username, setUsername] = React.useState();
+	const [email, setEmail] = React.useState();
+	const [password, setPassword] = React.useState();
 
-  return (
-    <View>
-      <TextInput
-      style = {styles.input}
-      placeholder = "First Name"
-      keyboardType = "default"
-      onChangeText = {onChangeText}
-      />
-      <Button
-      title = "Sign Up"
-      onPress = {() => console.log('Sign Up Button Pressed')}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.form}>
+			<TextInput
+				style={styles.input}
+				placeholder="First Name"
+				keyboardType="default"
+				value={firstName}
+				onChangeText={setFirstName}
+			/>
+			<TextInput
+				style={styles.input}
+				placeholder="Last Name"
+				keyboardType="default"
+				value={lastName}
+				onChangeText={setLastName}
+			/>
+			<TextInput
+				style={styles.input}
+				placeholder="Username"
+				keyboardType="default"
+				value={username}
+				onChangeText={setUsername}
+			/>
+			<TextInput
+				style={styles.input}
+				placeholder="Email"
+				keyboardType="default"
+				value={email}
+				onChangeText={setEmail}
+			/>
+			<TextInput
+				style={styles.input}
+				placeholder="Password"
+				keyboardType="default"
+				value={password}
+				onChangeText={setPassword}
+				autoCapitalize="none"
+				secureTextEntry={true}
+			/>
+			<Button title="Sign Up" onPress={() => console.log('Sign Up Button Pressed')} />
+		</View>
+	);
 };
 
-const styles =  StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-  },
+const styles = StyleSheet.create({
+	form: {
+		margin: 20,
+		backgroundColor: 'pink',
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+	},
 });
 
 export default SignUp;
