026859c30043a246c6c60e1721dcb8298130d929
Louise Ballantyne <lousballantyne@icloud.com>
2021-05-11 16:23:07 +0100

    A single working text field with no formatting on the Signup page

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import SignUp from './components/signup';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -9,18 +10,16 @@ export default function App() {
 }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <SignUp/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
