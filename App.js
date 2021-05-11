85aab6ee65fc22a553518c84eae6a3888917d44f
Sha <cspoppuppy@hotmail.com>
2021-05-11 16:56:33 +0100

    sign up form

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import SignUp from './components/signup';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
-  React.useEffect(() => {
-  fetch("/api")
-  .then((res) => res.json());
-}, []);
+	React.useEffect(() => {
+		fetch('/api').then((res) => res.json());
+	}, []);
 
-  return (
-    <View>
-      <SignUp/>
-    </View>
-  );
+	return (
+		<SafeAreaView>
+			<SignUp />
+		</SafeAreaView>
+	);
 }
 
 // const styles = StyleSheet.create({
