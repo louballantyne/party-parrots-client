d1eb4981ee1304a45d6dc2fe15808e0c497415c3
Sha <cspoppuppy@hotmail.com>
2021-05-12 21:28:05 +0100

    fix get parrots and cors

--- components/parrotList.js
+++ components/parrotList.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
-import useParrots from '../hooks/useParrots';
 import Parrot from './parrot';
 
-// import { useNavigation } from '@react-navigation/core';
-
 const ParrotList = () => {
-	const [parrots] = useParrots();
-	console.log('after get parrots: ', parrots);
+	const [parrots, setParrots] = useState([]);
+
+	useEffect(() => {
+		console.log('fetch data in use effect');
+		const res = fetch(`http://localhost:3000/api/parrots`, {
+			method: 'GET',
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				// console.log('inside response: ', data);
+				setParrots(data);
+			});
+	}, []);
 
 	return (
 		<View>
 			{parrots.map((parrot) => (
-				<Parrot key={parrot.id} name={parrot.name} />
+				<Parrot key={parrot._id} name={parrot.name} />
 			))}
 		</View>
 	);
