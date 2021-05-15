f2a1596ed5d134bbc0cb456705d548285f6da70d
Louise Ballantyne <lousballantyne@icloud.com>
2021-05-15 19:50:07 +0100

    Revert "Added radio buttons for user type"
    This reverts commit 5b154fbbefcd3e51f2a765f4449cb2231c9040e9.

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,19 +1,17 @@
 import React, { useState } from 'react';
-import { TextInput, Button, View, Text } from 'react-native';
+import { TextInput, Button, View } from 'react-native';
 import styles from '../../styles';
-import { RadioButton } from 'react-native-paper';
 
 const SignUp = ({ navigation }) => {
 	const [firstName, setFirstName] = useState();
 	const [lastName, setLastName] = useState();
 	const [username, setUsername] = useState();
 	const [email, setEmail] = useState();
-	const [userType, setUserType] = useState();
 	const [password, setPassword] = useState();
 
 
 	const onSignUpButtonClicked = async () => {
-		console.log('input fields: ', firstName, lastName, username, email, password, userType);
+		console.log('input fields: ', firstName, lastName, username, email, password);
 		await fetch(`http://localhost:3000/api/users`, {
 			method: 'POST',
 			headers: {
@@ -25,12 +23,12 @@ const SignUp = ({ navigation }) => {
 				username: username,
 				email: email,
 				password: password,
-				type: userType,
+				type: 'admin',
 			}),
 		})
 		.then((response) => response.json())
 		.then(data => {
-			console.log(data)
+			console.log(data) 
 			if (data.username === username) {
 				navigation.navigate('Parrot List');
 			}
@@ -84,18 +82,6 @@ const SignUp = ({ navigation }) => {
 				autoCapitalize="none"
 				secureTextEntry={true}
 			/>
-			<Text>Administrator</Text>
-			<RadioButton
-				value = "Administrator"
-				status = { userType === 'admin' ? 'checked' : 'unchecked' }
-				onPress={() => setUserType('admin')}
-				/>
-			<Text>Standard User</Text>
-			<RadioButton
-				value = "Standard"
-				status = { userType === 'standard' ? 'checked' : 'unchecked' }
-				onPress={() => setUserType('standard')}
-				/>
 			<Button title="Sign Up" onPress={() => onSignUpButtonClicked()} />
 		</View>
 	);
