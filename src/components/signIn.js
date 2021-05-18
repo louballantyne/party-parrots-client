3f385215365b5110ce91c60d45d22860b002f48e
Flora Davies <daviesfgo@gmail.com>
2021-05-18 16:36:34 +0100

    Added beautiful GIF to the logo and some colours for styling

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -1,76 +1,91 @@
-import React, { useState } from 'react';
-import { TextInput, Button, View, TouchableOpacity, Text, Alert } from 'react-native';
-import styles from '../../styles';
+import React, { useState } from "react";
+import {
+  TextInput,
+  Button,
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  Alert,
+} from "react-native";
+import styles from "../../styles";
 // import { Headbar } from '../../components/headbar/headbar'
-import { useNavigation } from '@react-navigation/core';
+import { useNavigation } from "@react-navigation/core";
 
 // might have to change this to just props and use navation const in function. WE SHALL SEE.
 const SignIn = (props) => {
-	const navigation = useNavigation();
+  const navigation = useNavigation();
 
-	const [userName, setUserName] = useState('');
-	const [passWord, setPassWord] = useState('');
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassWord] = useState("");
 
-	const onSignInButtonClicked = () => {
-		fetch('http://localhost:3000/api/sessions', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ username: userName, password: passWord }),
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				if (data.sessionId) {
-					props.navigation.navigate('Parrot List', {
-						userType: data.userType,
-						userId: data.userId,
-						sessionId: data.sessionId,
-					});
-					// a log in session function called here?
-				} else {
-					Alert.alert(data.message);
-				}
-			})
-			.catch((error) => console.error(error));
-	};
+  const onSignInButtonClicked = () => {
+    fetch("http://localhost:3000/api/sessions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: userName, password: passWord }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.sessionId) {
+          props.navigation.navigate("Parrot List", {
+            userType: data.userType,
+            userId: data.userId,
+            sessionId: data.sessionId,
+          });
+          // a log in session function called here?
+        } else {
+          Alert.alert(data.message);
+        }
+      })
+      .catch((error) => console.error(error));
+  };
 
-	const keyPressed = (event) => {
-		if (event.key === 'Enter') {
-			onSignInButtonClicked();
-		}
-	};
+  const keyPressed = (event) => {
+    if (event.key === "Enter") {
+      onSignInButtonClicked();
+    }
+  };
 
-	return (
-		<View>
-			{/* <Headbar {...props}/> */}
-			<View style={styles.inputForm}>
-				<TextInput
-					style={styles.inputField}
-					autoCapitalize="none"
-					placeholder="username"
-					type="text"
-					onChangeText={(text) => setUserName(text)}
-					value={userName}
-				/>
-				<TextInput
-					style={styles.inputField}
-					autoCapitalize="none"
-					secureTextEntry={true}
-					placeholder="password"
-					onChangeText={(text) => setPassWord(text)}
-					onKeyPress={(key) => keyPressed(key)}
-					value={passWord}
-				/>
-				<Button onPress={() => onSignInButtonClicked()} title="Sign In" />
-				<Button
-					style={styles.button}
-					onPress={() => navigation.navigate('Sign Up')}
-					title="Create an account!"
-				/>
-			</View>
-		</View>
-	);
+  return (
+    <View style={styles.body}>
+      <View style={styles.circleImageContainer}>
+        <Image
+          source={require("../images/gify-parrot.gif")}
+          style={styles.circleImage}
+        />
+      </View>
+
+      {/* <Headbar {...props}/> */}
+      <View style={styles.inputForm}>
+        <TextInput
+          style={styles.inputField}
+          autoCapitalize="none"
+          placeholder="username"
+          type="text"
+          onChangeText={(text) => setUserName(text)}
+          value={userName}
+        />
+        <TextInput
+          style={styles.inputField}
+          autoCapitalize="none"
+          secureTextEntry={true}
+          placeholder="password"
+          onChangeText={(text) => setPassWord(text)}
+          onKeyPress={(key) => keyPressed(key)}
+          value={passWord}
+        />
+        <Button onPress={() => onSignInButtonClicked()} title="Sign In" />
+        <Button
+          style={styles.button}
+          onPress={() => navigation.navigate("Sign Up")}
+          title="Create an account!"
+        />
+      </View>
+    </View>
+  );
 };
 
 export { SignIn };
--- styles.js
+++ styles.js
@@ -1,115 +1,132 @@
-import { Dimensions, StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-	inputForm: {
-		margin: 20,
-		// backgroundColor: 'pink',
-	},
-	inputField: {
-		height: 40,
-		margin: 12,
-		paddingLeft: 5,
-		borderWidth: 1,
-		backgroundColor: 'white',
-	},
-	itemContainer: {
-		flex: 1,
-		flexDirection: 'row',
-	},
-	itemInfoContainer: {
-		flexDirection: 'column',
-		marginTop: 10,
-	},
-	dropDown: {
-		width: 100,
-		height: 100,
-		margin: 5,
-		flex: 1,
-		paddingTop: 40,
-		alignItems: 'center',
-	},
-	header: {
-		alignSelf: 'stretch',
-		height: 52,
-		flexDirection: 'row', // row
-		backgroundColor: '#179aff',
-		alignItems: 'flex-end',
-		justifyContent: 'space-between', // center, space-around
-		paddingLeft: 10,
-		paddingRight: 10,
-	},
-	profileImageContainer: {
-		width: 100,
-		height: 100,
-		marginTop: 10,
-		marginBottom: 10,
-		alignSelf: 'center',
-	},
-	profileImage: {
-		flex: 1,
-		resizeMode: 'contain',
-	},
-	iconImageContainer: {
-		width: 100,
-		height: 100,
-		margin: 10,
-	},
-	iconImage: {
-		flex: 1,
-		width: 100,
-		height: 100,
-		// resizeMode: 'contain',
-		// borderRadius: 100,
-	},
-	parrotInfo: {
-		margin: 10,
-	},
-	parrotApplications: {
-		margin: 5,
-	},
-	infoItem: {
-		flexDirection: 'row',
-		margin: 2,
-	},
-	infoLabel: {
-		fontWeight: 'bold',
-	},
-	applicationsContainer: {
-		margin: 10,
-	},
-	applicationContainer: {
-		flexDirection: 'row',
-		margin: 5,
-	},
-	approveButton: {
-		marginLeft: 10,
-	},
-	redBoldFont: {
-		color: 'red',
-		fontWeight: 'bold',
-	},
-	mapViewContainer: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	mapView: {
-		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height,
-	},
-	locationMapContainer: {
-		width: Dimensions.get('window').width,
-		height: 100,
-		backgroundColor: '#fff',
-		alignSelf: 'center',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	locationMap: {
-		width: Dimensions.get('window').width,
-		height: 100,
-	},
+  body: {
+    // navbar #80b383
+    backgroundColor: "#c5e3c7",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputForm: {
+    margin: 20,
+    // backgroundColor: 'pink',
+  },
+  inputField: {
+    height: 40,
+    margin: 12,
+    paddingLeft: 5,
+    borderWidth: 0,
+    backgroundColor: "#c5e3c7",
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  itemInfoContainer: {
+    flexDirection: "column",
+    marginTop: 10,
+  },
+  dropDown: {
+    width: 100,
+    height: 100,
+    margin: 5,
+    flex: 1,
+    paddingTop: 40,
+    alignItems: "center",
+  },
+  header: {
+    alignSelf: "stretch",
+    height: 52,
+    flexDirection: "row", // row
+    backgroundColor: "#179aff",
+    alignItems: "flex-end",
+    justifyContent: "space-between", // center, space-around
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  profileImageContainer: {
+    width: 100,
+    height: 100,
+    marginTop: 10,
+    marginBottom: 10,
+    alignSelf: "center",
+  },
+  profileImage: {
+    flex: 1,
+    resizeMode: "contain",
+  },
+  iconImageContainer: {
+    width: 100,
+    height: 100,
+    margin: 10,
+  },
+  iconImage: {
+    flex: 1,
+    width: 100,
+    height: 100,
+    // resizeMode: 'contain',
+    // borderRadius: 100,
+  },
+  parrotInfo: {
+    margin: 10,
+  },
+  parrotApplications: {
+    margin: 5,
+  },
+  infoItem: {
+    flexDirection: "row",
+    margin: 2,
+  },
+  infoLabel: {
+    fontWeight: "bold",
+  },
+  applicationsContainer: {
+    margin: 10,
+  },
+  applicationContainer: {
+    flexDirection: "row",
+    margin: 5,
+  },
+  approveButton: {
+    marginLeft: 10,
+  },
+  redBoldFont: {
+    color: "red",
+    fontWeight: "bold",
+  },
+  mapViewContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapView: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  locationMapContainer: {
+    width: Dimensions.get("window").width,
+    height: 100,
+    backgroundColor: "#fff",
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  locationMap: {
+    width: Dimensions.get("window").width,
+    height: 100,
+  },
+  circleImageContainer: {
+    marginTop: 20,
+    width: 300,
+    height: 300,
+    alignSelf: "center",
+  },
+  circleImage: {
+    width: 300,
+    height: 300,
+    borderRadius: 300,
+  },
 });
 
 export default styles;
