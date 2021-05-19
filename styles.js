c2a39ce418b1b0495b85005356d90c78acf91464
Louise Ballantyne <lousballantyne@icloud.com>
2021-05-19 10:44:56 +0100

    Reordered stylesheet

--- styles.js
+++ styles.js
@@ -2,95 +2,6 @@ import { Dimensions, StyleSheet } from 'react-native';
 import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 const styles = StyleSheet.create({
-  approvedItemContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    margin: 10,
-    borderRadius: 10,
-  },
-  formBody: {
-    // navbar #80b383
-    backgroundColor: '#c5e3c7',
-    justifyContent: 'center',
-    alignItems: 'center',
-    },
-  pageBody: {
-    backgroundColor: '#c5e3c7',
-    },
-  body: {
-    // navbar #80b383
-    //backgroundColor: "#c5e3c7",
-    justifyContent: "center",
-    flex: 1,
-    alignItems: "center",
-  },
-  inputForm: {
-    //margin: 20,
-    // backgroundColor: 'pink',
-    alignItems: "center",
-    width: Dimensions.get('window').width * 0.7,
-  },
-  inputField: {
-    height: 40,
-    margin: 12,
-    //marginBottom: 12,
-    paddingLeft: 5,
-    borderWidth: 0,
-    backgroundColor: "white",
-    width: "50%",
-    //borderRadius: 10,
-  },
-  itemContainer: {
-    flex: 1,
-    flexDirection: "row",
-    //backgroundColor: 'white',
-		//margin: 10,
-		//borderRadius: 10,
-  },
-  itemInfoContainer: {
-    flexDirection: "column",
-    marginTop: 10,
-  },
-  profileImageContainer: {
-    width: 100,
-    height: 100,
-    marginTop: 10,
-    marginBottom: 10,
-    alignSelf: "center",
-  },
-  profileImage: {
-    flex: 1,
-    resizeMode: "contain",
-  },
-  iconImageContainer: {
-    width: 100,
-    height: 100,
-    margin: 10,
-  },
-  iconImage: {
-    flex: 1,
-    width: 100,
-    height: 100,
-    // resizeMode: 'contain',
-    // borderRadius: 100,
-  },
-  parrotInfo: {
-    margin: 10,
-  },
-  parrotApplications: {
-    margin: 5,
-  },
-  infoItem: {
-    flexDirection: "row",
-    margin: 2,
-  },
-  infoLabel: {
-    fontWeight: "bold",
-  },
-  applicationsContainer: {
-    margin: 10,
-  },
   applicationContainer: {
     flexDirection: "row",
     margin: 5,
@@ -98,49 +9,35 @@ const styles = StyleSheet.create({
   applicationContent: {
 		width: Dimensions.get('window').width * 0.7,
 	},
+	applicationsContainer: {
+    margin: 10,
+  },
   approveButton: {
     marginLeft: 10,
   },
-  redBoldFont: {
-    color: "red",
-    fontWeight: "bold",
-  },
-  mapViewContainer: {
+  approvedItemContainer: {
     flex: 1,
-    backgroundColor: "#fff",
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    margin: 10,
+    borderRadius: 10,
+  },
+	body: {
+    // navbar #80b383
+    //backgroundColor: "#c5e3c7",
+    justifyContent: "center",
+    flex: 1,
     alignItems: "center",
-    justifyContent: "center",
   },
-  mapView: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-  locationMapContainer: {
-    width: Dimensions.get("window").width,
-    height: 100,
-    backgroundColor: "#fff",
+	button: {
+    padding: 10,
+    borderRadius: 50,
     alignSelf: "center",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#83ba85",
+    margin: 4,
+    width: "43%",
   },
-  locationMap: {
-    width: Dimensions.get("window").width,
-    height: 100,
-  },
-  circleImageContainer: {
-		marginTop: 20,
-		width: Dimensions.get('window').width * 0.6,
-		height: Dimensions.get('window').width * 0.6,
-		alignSelf: 'center',
-	},
-  circleImage: {
-    width: Dimensions.get('window').width * 0.6,
-    height: Dimensions.get('window').width * 0.6,
-    borderRadius: Dimensions.get('window').width * 0.6,
-    alignSelf: "center",
-    justifyContent: "center",
-  },
-  buttonContainer: {
+	buttonContainer: {
 		margin: 20,
 		backgroundColor: '#80b383',
 		// width: Dimensions.get('window').width * 0.5,
@@ -153,10 +50,135 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
-  buttonText: {
+	buttonText: {
 		color: 'white',
 		fontSize: 14,
 		fontWeight: 'bold',
+	},
+	circleImageContainer: {
+		marginTop: 20,
+		width: Dimensions.get('window').width * 0.6,
+		height: Dimensions.get('window').width * 0.6,
+		alignSelf: 'center',
+	},
+	circleImage: {
+		width: Dimensions.get('window').width * 0.6,
+		height: Dimensions.get('window').width * 0.6,
+		borderRadius: Dimensions.get('window').width * 0.6,
+		alignSelf: "center",
+		justifyContent: "center",
+	},
+  formBody: {
+    // navbar #80b383
+    backgroundColor: '#c5e3c7',
+    justifyContent: 'center',
+    alignItems: 'center',
+    },
+	iconImage: {
+    flex: 1,
+    width: 100,
+    height: 100,
+    // resizeMode: 'contain',
+    // borderRadius: 100,
+  },
+  iconImageContainer: {
+    width: 100,
+    height: 100,
+    margin: 10,
+  },
+  infoItem: {
+    flexDirection: "row",
+    margin: 2,
+  },
+  infoLabel: {
+    fontWeight: "bold",
+  },
+	inputForm: {
+		//margin: 20,
+		// backgroundColor: 'pink',
+		alignItems: "center",
+		width: Dimensions.get('window').width * 0.7,
+	},
+	inputField: {
+		height: 40,
+		margin: 12,
+		//marginBottom: 12,
+		paddingLeft: 5,
+		borderWidth: 0,
+		backgroundColor: "white",
+		width: "50%",
+		//borderRadius: 10,
+	},
+	itemContainer: {
+		flex: 1,
+		flexDirection: "row",
+		//backgroundColor: 'white',
+		//margin: 10,
+		//borderRadius: 10,
+	},
+  itemInfoContainer: {
+    flexDirection: "column",
+    marginTop: 10,
+  },
+	locationMap: {
+		width: Dimensions.get("window").width,
+		height: 100,
+	},
+	locationMapContainer: {
+		width: Dimensions.get("window").width,
+		height: 100,
+		backgroundColor: "#fff",
+		alignSelf: "center",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+  mapViewContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapView: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+	pageBody: {
+    backgroundColor: '#c5e3c7',
+  },
+	parrotInfo: {
+		margin: 10,
+	},
+	parrotApplications: {
+		margin: 5,
+	},
+  profileImageContainer: {
+    width: 100,
+    height: 100,
+    marginTop: 10,
+    marginBottom: 10,
+    alignSelf: "center",
+  },
+  profileImage: {
+    flex: 1,
+    resizeMode: "contain",
+  },
+	redBoldFont: {
+		color: "red",
+		fontWeight: "bold",
+	},
+	signInButton: {
+		padding: 10,
+		borderRadius: 50,
+		alignSelf: "center",
+		backgroundColor: "#83ba85",
+		margin: 4,
+		width: "50%",
+	},
+	signOutButton: {
+		color: "#bf04a3",
+		fontSize: 18,
+		fontWeight: "bold",
+		margin: 10,
 	},
   smallButtonContainer: {
 		margin: 20,
@@ -182,38 +204,12 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     justifyContent: "center",
   },
-  
   title: {
     color: "#bf04a3",
     fontSize: 30,
     fontWeight: "bold",
     alignSelf: "center",
     justifyContent: "center",
-  },
-
-  button: {
-    padding: 10,
-    borderRadius: 50,
-    alignSelf: "center",
-    backgroundColor: "#83ba85",
-    margin: 4,
-    width: "43%",
-  },
-
-  signInButton: {
-    padding: 10,
-    borderRadius: 50,
-    alignSelf: "center",
-    backgroundColor: "#83ba85",
-    margin: 4,
-    width: "50%",
-  },
-
-  signOutButton: {
-    color: "#bf04a3",
-    fontSize: 18,
-    fontWeight: "bold",
-    margin: 10,
   },
 });
 
