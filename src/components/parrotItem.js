c2ee0120acbd6b1ce5135c12eb6c922d63d4ef2e
Flora Davies <daviesfgo@gmail.com>
2021-05-19 10:46:30 +0100

    picture and sign out button removed

--- src/components/parrotItem.js
+++ src/components/parrotItem.js
@@ -1,39 +1,62 @@
-import React, { useState } from 'react';
-import { View, Image, Text, ImageBackground } from 'react-native';
-import styles from '../../styles';
-import { ParrotInfoItem } from './parrotInfoItem';
+import React, { useState } from "react";
+import { View, Image, Text, ImageBackground } from "react-native";
+import styles from "../../styles";
+import { ParrotInfoItem } from "./parrotInfoItem";
 
-const Parrot = ({ parrotId, name, location, age, gender, imgUrl, approved, navigation, userType, userId }) => {
-	const onViewClicked = () => {
-		navigation.navigate('Parrot Page', { parrotId: parrotId, userType: userType, userId: userId });
-	};
+const Parrot = ({
+  parrotId,
+  name,
+  location,
+  age,
+  gender,
+  imgUrl,
+  approved,
+  navigation,
+  userType,
+  userId,
+}) => {
+  const onViewClicked = () => {
+    navigation.navigate("Parrot Page", {
+      parrotId: parrotId,
+      userType: userType,
+      userId: userId,
+    });
+  };
 
-	return (
-		<View
-			style={approved ? styles.approvedItemContainer : styles.itemContainer}
-			onStartShouldSetResponder={() => onViewClicked()}
-		>
-			{!approved && (
-				<View style={styles.iconImageContainer}>
-					<Image style={styles.iconImage} source={{ uri: imgUrl }}></Image>
-				</View>
-			)}
+  return (
+    <View
+      style={approved ? styles.approvedItemContainer : styles.itemContainer}
+      onStartShouldSetResponder={() => onViewClicked()}
+    >
+      {!approved && (
+        <View style={styles.iconImageContainer}>
+          <Image style={styles.iconImage} source={{ uri: imgUrl }}></Image>
+        </View>
+      )}
 
-			{approved && (
-				<View style={styles.iconImageContainer}>
-					<ImageBackground style={styles.iconImage} source={{ uri: imgUrl }} blurRadius={5}>
-						<Image style={styles.iconImage} source={require('../images/forbidden.png')} blurRadius={3} />
-					</ImageBackground>
-				</View>
-			)}
-			<View style={styles.itemInfoContainer}>
-				<ParrotInfoItem label="Name: " info={name} />
-				<ParrotInfoItem label="Age: " info={age} />
-				<ParrotInfoItem label="Gender: " info={gender} />
-				<ParrotInfoItem label="Location: " info={location} />
-			</View>
-		</View>
-	);
+      {approved && (
+        <View style={styles.iconImageContainer}>
+          <ImageBackground
+            style={styles.iconImage}
+            source={{ uri: imgUrl }}
+            blurRadius={5}
+          >
+            <Image
+              style={styles.iconImage}
+              source={require("../images/rehomed.png")}
+              blurRadius={3}
+            />
+          </ImageBackground>
+        </View>
+      )}
+      <View style={styles.itemInfoContainer}>
+        <ParrotInfoItem label="Name: " info={name} />
+        <ParrotInfoItem label="Age: " info={age} />
+        <ParrotInfoItem label="Gender: " info={gender} />
+        <ParrotInfoItem label="Location: " info={location} />
+      </View>
+    </View>
+  );
 };
 
 export default Parrot;
--- src/components/parrotList.js
+++ src/components/parrotList.js
@@ -12,28 +12,28 @@ import Parrot from "./parrotItem";
 import styles from "../../styles";
 
 const ParrotList = ({ navigation, route }) => {
-	const [parrots, setParrots] = useState([]);
-	const [approvedApplications, setApprovedApplications] = useState([]);
-	const { userType, userId, sessionId } = route.params;
+  const [parrots, setParrots] = useState([]);
+  const [approvedApplications, setApprovedApplications] = useState([]);
+  const { userType, userId, sessionId } = route.params;
 
-	useEffect(() => {
-		const fetchParrots = async () => {
-			console.log('fetch data in use effect');
-			const res = await fetch(`http://localhost:3000/api/parrots`, {
-				method: 'GET',
-			})
-				.then((response) => response.json())
-				.then((data) => {
-					setParrots(data.parrots);
-					setApprovedApplications(data.approvedApplications);
-				});
-		};
-		fetchParrots();
-	}, []);
+  useEffect(() => {
+    const fetchParrots = async () => {
+      console.log("fetch data in use effect");
+      const res = await fetch(`http://localhost:3000/api/parrots`, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setParrots(data.parrots);
+          setApprovedApplications(data.approvedApplications);
+        });
+    };
+    fetchParrots();
+  }, []);
 
-	// const getParrotsApprovedStatus = () => {
-	// 	parrots.forEach((parrot) => (parrot.approved = isParrotApproved(parrot._id)));
-	// };
+  // const getParrotsApprovedStatus = () => {
+  // 	parrots.forEach((parrot) => (parrot.approved = isParrotApproved(parrot._id)));
+  // };
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
@@ -45,80 +45,84 @@ const ParrotList = ({ navigation, route }) => {
     });
   }, [navigation, onSignOutButtonClicked]);
 
-	const isParrotApproved = (parrotId) => {
-		const parrotApproved = approvedApplications.filter((application) => application.parrot === parrotId);
-		return parrotApproved.length > 0;
-	};
+  const isParrotApproved = (parrotId) => {
+    const parrotApproved = approvedApplications.filter(
+      (application) => application.parrot === parrotId
+    );
+    return parrotApproved.length > 0;
+  };
 
-	const onSignOutButtonClicked = async () => {
-		console.log('sign out: ', `http://localhost:3000/api/sessions/${sessionId}`);
-		await fetch(`http://localhost:3000/api/sessions/${sessionId}`, {
-			method: 'DELETE',
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				Alert.alert(data.message);
-				if (data.message === 'Successfully Logged Out') navigation.navigate('Sign In');
-			})
-			.catch((error) => console.error(error));
-	};
+  const onSignOutButtonClicked = async () => {
+    console.log(
+      "sign out: ",
+      `http://localhost:3000/api/sessions/${sessionId}`
+    );
+    await fetch(`http://localhost:3000/api/sessions/${sessionId}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        Alert.alert(data.message);
+        if (data.message === "Successfully Logged Out")
+          navigation.navigate("Sign In");
+      })
+      .catch((error) => console.error(error));
+  };
 
-	return (
-		<View style={styles.pageBody}>
-			<View>
-				{userType === 'admin' && (
-					<Button
-						title="Add Parrot"
-						onPress={() =>
-							navigation.navigate('Add Parrot', {
-								userId: userId,
-								userType: userType,
-								sessionId: sessionId,
-							})
-						}
-					/>
-				)}
-				{userType !== 'admin' && (
-					<Button
-						title="Map View"
-						onPress={() =>
-							navigation.navigate('Map View', {
-								userType: userType,
-								userId: userId,
-							})
-						}
-					/>
-				)}
-				<Button title="Sign Out" onPress={() => onSignOutButtonClicked()} />
-			</View>
-			<FlatList
-				data={parrots}
-				renderItem={({ item }) =>
-					(userType !== 'admin' || userId === item.user) && (
-						<Parrot
-							key={item._id}
-							parrotId={item._id}
-							name={item.name}
-							location={item.location}
-							age={item.age}
-							gender={item.gender}
-							imgUrl={
-								item.imageUrl
-									? item.imageUrl
-									: 'https://party-parrots-s3-bucket.s3.amazonaws.com/parrot.jpeg'
-							}
-							approved={isParrotApproved(item._id)}
-							navigation={navigation}
-							userType={userType}
-							userId={userId}
-						/>
-					)
-				}
-				keyExtractor={(item) => item._id}
-			/>
-		</View>
-	);
-
+  return (
+    <View style={styles.pageBody}>
+      <View>
+        {userType === "admin" && (
+          <Button
+            title="Add Parrot"
+            onPress={() =>
+              navigation.navigate("Add Parrot", {
+                userId: userId,
+                userType: userType,
+                sessionId: sessionId,
+              })
+            }
+          />
+        )}
+        {userType !== "admin" && (
+          <Button
+            title="Map View"
+            onPress={() =>
+              navigation.navigate("Map View", {
+                userType: userType,
+                userId: userId,
+              })
+            }
+          />
+        )}
+      </View>
+      <FlatList
+        data={parrots}
+        renderItem={({ item }) =>
+          (userType !== "admin" || userId === item.user) && (
+            <Parrot
+              key={item._id}
+              parrotId={item._id}
+              name={item.name}
+              location={item.location}
+              age={item.age}
+              gender={item.gender}
+              imgUrl={
+                item.imageUrl
+                  ? item.imageUrl
+                  : "https://party-parrots-s3-bucket.s3.amazonaws.com/parrot.jpeg"
+              }
+              approved={isParrotApproved(item._id)}
+              navigation={navigation}
+              userType={userType}
+              userId={userId}
+            />
+          )
+        }
+        keyExtractor={(item) => item._id}
+      />
+    </View>
+  );
 };
 
 export { ParrotList };
