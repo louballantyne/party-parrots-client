215ec6188e20d3d586d9af38e897563cd1448361
Sha <cspoppuppy@hotmail.com>
2021-05-20 08:51:41 +0100

    amend map in add parrot

--- src/components/parrotLocationMap.js
+++ src/components/parrotLocationMap.js
@@ -1,33 +1,34 @@
-import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
-import MapView, { Marker, Callout } from "react-native-maps";
-import styles from "../../styles";
+import React, { useState, useEffect } from 'react';
+import { View, Image } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import styles from '../../styles';
 
 const ParrotLocationMap = ({ geocode }) => {
-  return (
-    <View style={styles.locationMapContainer}>
-      <MapView
-        style={styles.locationMap}
-        initialRegion={{
-          latitude: geocode.latitude,
-          longitude: geocode.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker
-          coordinate={{
-            latitude: geocode.latitude,
-            longitude: geocode.longitude,
-          }}
-        >
-          {/* <Callout>
+	return (
+		<View style={styles.locationMapContainer}>
+			<MapView
+				style={styles.locationMap}
+				initialRegion={{
+					latitude: geocode.latitude + 1.2,
+					longitude: geocode.longitude - 1.3,
+					latitudeDelta: 4.0922,
+					longitudeDelta: 10.0421,
+				}}
+			>
+				<Marker
+					coordinate={{
+						latitude: geocode.latitude,
+						longitude: geocode.longitude,
+					}}
+				>
+					<Image source={require('../images/party-parrot2.png')} style={styles.mapViewPinImage} />
+					{/* <Callout>
             <Text>{parrot.name + ': ' + parrot.location}</Text>
           </Callout> */}
-        </Marker>
-      </MapView>
-    </View>
-  );
+				</Marker>
+			</MapView>
+		</View>
+	);
 };
 
 export { ParrotLocationMap };
